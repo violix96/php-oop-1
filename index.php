382c7c6e538aecb680a280dfc437c43f84cdb0ee
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-06-24 16:30:50 +0200

    [UPD] creating movie instances

--- index.php
+++ index.php
@@ -31,3 +31,11 @@ class Movie
         return $this->rating;
     }
 }
+
+// creazione delle istanze di movie. 
+
+$inception = new Movie('Inception', 'Sci-Fi', 8.8);
+$LaLeggendaDiAlJohneJack = new Movie('La leggenda di Al, John e Jack ', 'Commedy', 7.1);
+$FightClub = new Movie('Fight Club ', 'Drama', 8.4);
+$ToroScatenato = new Movie('Toro scatenato ', 'Drama', 10);
+$QueiBraviRagazzi = new Movie('Quei bravi ragazzi', 'Drama', 9.9);
