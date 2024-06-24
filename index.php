d86effb3e2552721604cede05c64e1c4cb34cea1
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-06-24 16:45:18 +0200

    [UPD] printed movie information in index.php and completed exercise

--- index.php
+++ index.php
@@ -39,3 +39,25 @@ $LaLeggendaDiAlJohneJack = new Movie('La leggenda di Al, John e Jack ', 'Commedy
 $FightClub = new Movie('Fight Club ', 'Drama', 8.4);
 $ToroScatenato = new Movie('Toro scatenato ', 'Drama', 10);
 $QueiBraviRagazzi = new Movie('Quei bravi ragazzi', 'Drama', 9.9);
+
+// stampa delle informazioni per ogni film 
+
+echo 'Film: ' . $inception->getTitle() . "<br>";
+echo "Categoria: " . $inception->getCategory() . "<br>";
+echo "Punteggio: " . $inception->getRating() . "<br><br>";
+
+echo 'Film: ' . $LaLeggendaDiAlJohneJack->getTitle() . "<br>";
+echo "Categoria: " . $LaLeggendaDiAlJohneJack->getCategory() . "<br>";
+echo "Punteggio: " . $LaLeggendaDiAlJohneJack->getRating() . "<br><br>";
+
+echo 'Film: ' . $FightClub->getTitle() . "<br>";
+echo "Categoria: " . $FightClub->getCategory() . "<br>";
+echo "Punteggio: " . $FightClub->getRating() . "<br><br>";
+
+echo 'Film: ' . $ToroScatenato->getTitle() . "<br>";
+echo "Categoria: " . $ToroScatenato->getCategory() . "<br>";
+echo "Punteggio: " . $ToroScatenato->getRating() . "<br><br>";
+
+echo 'Film: ' . $QueiBraviRagazzi->getTitle() . "<br>";
+echo "Categoria: " . $QueiBraviRagazzi->getCategory() . "<br>";
+echo "Punteggio: " . $QueiBraviRagazzi->getRating() . "<br><br>";
