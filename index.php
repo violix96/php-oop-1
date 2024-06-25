52b6559959b1d2152fa6b3ff3d13a810597db11b
Vincenzo Fabrizio Viola <vincy1621@gmail.com>
2024-06-25 10:59:09 +0200

    [UPD] created Models folder

--- index.php
+++ index.php
@@ -1,54 +1,8 @@
 <?php
 
-class Movie
-{
-    private $title;
-    private $category;
-    private $rating;
+require_once __DIR__ . '/MODELS/Movie.php';
 
-    public function __construct($title, $category, $rating)
-    {
-        $this->title = $title;
-        $this->category = $category;
-        $this->rating = $rating;
-    }
 
-    // metodi getter per ottenere le informazioni del film
-    public function getTitle(): string
-    {
-        return $this->title;
-    }
-
-    // metodo setter per impostare il titolo
-    public function setTitle(string $title): void
-    {
-        $this->title = $title;
-    }
-
-    // metodo getter per ottenere la categoria
-    public function getCategory(): string
-    {
-        return $this->category;
-    }
-
-    // metodo setter per impostare la categoria
-    public function setCategory(string $category): void
-    {
-        $this->category = $category;
-    }
-
-    // metodo getter per ottenere il punteggio
-    public function getRating(): float
-    {
-        return $this->rating;
-    }
-
-    // metodo setter per impostare il punteggio
-    public function setRating(float $rating): void
-    {
-        $this->rating = $rating;
-    }
-}
 
 
 // creazione delle istanze di movie. 
